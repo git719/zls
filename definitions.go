da2fbb3a7f41d81c249c08ed99cd6b926cefe565
Joe Coder <104685438+git719@users.noreply.github.com>
2022-10-01 10:42:02 -0400

    definitions searches

--- definitions.go
+++ definitions.go
@@ -78,13 +78,47 @@ func PrintRoleDefinition(x map[string]interface{}) {
 }
 
 func GetRoleDefinitions() (oList []interface{}) {
-	// Get all role definitions from Azure.
-	// Ref: https://docs.microsoft.com/en-us/azure/role-based-access-control/role-definitions-list
+	// Get all role definitions that are available to use in current tenant 
+
+	// IMPORTANT: Presently (October 2022), the RBAC API $filter=AtScopeAndBelow() does NOT work as
+    // documented at https://learn.microsoft.com/en-us/azure/role-based-access-control/role-definitions-list.
+    // This means that anyone searching for a comprehensive list of ALL role definitions within an Azure tenant
+    // is forced to do this in a piecemeal, cumulative fashion. The search must start at the Tenant Root Group
+    // scope level, then proceed through EACH lower subscope within the hierarchy. This entails building a list
+    // of all MGs at the root and searching for all roles DEFINED there; then building a list of all subscriptions
+    // within each of those MGs and gettting all roles defined in them; then repeating for all resource groups
+    // within each subscription, and so on. The hierarchy is something like:
+    
+    //   PATH               EXAMPLE
+    //   Tenant Root Group  /providers/Microsoft.Management/managementGroups/{myTenantId}
+    //   Management Group   /providers/Microsoft.Management/managementGroups/{groupId1}
+    //   Subscription       /subscriptions/{subscriptionId1}
+    // 	Resource Group     /subscriptions/{subscriptionId1}/resourceGroups/{myResourceGroup1}
+    // 	Resource           /subscriptions/{subscriptionId1}/resourceGroups/{myResourceGroup1}/providers/Microsoft.Web/sites/mySite1
+    
+    // Note that because Microsoft Azure BUILT-IN roles are defined universally, at scope "/", they are all
+    // gathered on the initial Tenant Root Group search. That means these comments are predominantly about CUSTOM
+    // roles that a customer is likely to define within their respective tenant.
+    
+    // The best practice a customer can follow is to define ALL of their custom roles as universally as possible,
+    // at the highest scope, the Tenant Root Group scope. That way, they are "visible" and therefore consumable
+    // anywhere witin the tenant. As of this writing, Microsoft Azure still has a limitation whereby any custom
+    // role having DataAction or NotDataAction CANNOT be defined at any MG scope level, and that prevents this
+    // good practice. Microsoft is actively working to lift this restriction, see: 
+    // https://learn.microsoft.com/en-us/azure/role-based-access-control/custom-roles
+
+    // There may be customers out there who at some point decided to define some of their custom roles within some
+    // of the hidden subscopes, and that's the reason why this utility follows this search algorithm to gather
+    // the full list of roles definitions. Note however, that this utility ONLY searches as deep as subscriptions,
+    // so if there are role definitions hidden within Resource Groups or individual resoures it will MISS them.
+
+	// Step 1: Get all role definitions at the root tenant, including all Microsoft Azure Built-In-Roles
+    oList = [] 
 	oList = nil
 	var uuids []string // Keep track of each unique role definition to whittle out repeats that come up in lower scopes
 	url := az_url + "/providers/Microsoft.Management/managementGroups/" + tenant_id
 	url += "/providers/Microsoft.Authorization/roleDefinitions"
-	params := map[string]string{"filter": "atScopeAndBelow()"} // Get all definitions in the entire tenant
+	params := map[string]string{"filter": "atScopeAndBelow()"} // Again, this does not work as expected and documented
 	r := APIGet(url, az_headers, params, false)
 	if r["value"] != nil {
 		definitions := r["value"].([]interface{}) // Assert as JSON array type
@@ -96,9 +130,7 @@ func GetRoleDefinitions() (oList []interface{}) {
 		}
 	}
 
-	// STILL cannot seem to get all tenant role defs using atScopeAndBelow, so force to re-add below :-(
-
-	// Step2: Now get all CUSTOM role definitions under each subscription, using the uuids list to ensure uniqueness
+	// Step 2: Now get all CUSTOM roles under each subscription, ensuring uniqueness
 	subMap := GetIdNameMap("s") // To print sub names, not IDs
 	apiCalls := 1               // Track number of API calls below
 	for _, i := range GetSubIds() {
