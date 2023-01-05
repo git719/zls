361fb86117a7a0b4f938d230a15fabd6e9585899
Joe Coder <104685438+git719@users.noreply.github.com>
2023-01-05 13:15:21 -0500

    changes v188, 2

--- assignments.go
+++ assignments.go
@@ -78,7 +78,7 @@ func PrintRoleAssignment(x map[string]interface{}) {
 
 	subMap := GetIdNameMap("s")  // Get subscriptions id:name map
 	scope := StrVal(xProp["scope"])
-	if scope == "" { scope = StrVal(xProps["Scope"]) }  // Account for possibly capitalized key
+	if scope == "" { scope = StrVal(xProp["Scope"]) }  // Account for possibly capitalized key
 	if strings.HasPrefix(scope, "/subscriptions") {
 		split := strings.Split(scope, "/")
 		subName := subMap[split[2]]
